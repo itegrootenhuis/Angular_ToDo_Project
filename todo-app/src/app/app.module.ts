29b215ce25780e147850d9d038eab14cc9188a71
Ian TeGrootenhuis <itegrootenhuis@bizstream.com>
2020-07-09 14:50:26 -0400

    Removed boiler plate junk from app page and added chore component.

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChoreComponent } from './chore/chore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoreComponent
   ],
   imports: [
     BrowserModule,
---
+++ todo-app/src/app/chore/chore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chore',
+  templateUrl: './chore.component.html',
+  styleUrls: ['./chore.component.scss']
+})
+export class ChoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
